3086d1757979d2317804bd10fcade7df8149b8cc
Arron <arronzhang@me.com>
2019-04-17 16:54:03 +0800

    Export wework provider

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
-exports.Wework = require('./lib/wework')
 exports.AccessToken = require('./lib/access-token')
 exports.Error = require('./lib/error')
+exports.Wework = require('./lib/wework')
+exports.WeworkProvider = require('./lib/wework-provider')
