a0e3f8412f0dc79a5b00ce4e6b099b786c6c099e
Arron <arronzhang@me.com>
2019-04-20 13:34:25 +0800

    Stringify reply message

--- test/receiver.test.js
+++ test/receiver.test.js
@@ -4,7 +4,9 @@ const request = require('supertest')
 const qs = require('querystring')
 
 describe('parse', () => {
-  let token = '4c9184f37cff01bcdc32dc486ec36961'
+  let config = {
+    token: '4c9184f37cff01bcdc32dc486ec36961'
+  }
   test('validate get', () => {
     let query = {
       signature: 'b5c32d63ae3061aec855af7b638dcef72cb6f46a',
@@ -12,8 +14,8 @@ describe('parse', () => {
       timestamp: '1555644411',
       nonce: '1395122694'
     }
-    expect(Receiver.parse(token, null, query)).toHaveProperty('message', '4810926405170593787')
-    expect(Receiver.parse('abc', null, query)).toBeFalsy()
+    expect(Receiver.verify(config, query)).toHaveProperty('message', '4810926405170593787')
+    expect(Receiver.verify({ token: 'abc' }, query)).toBeFalsy()
   })
 
   test('validate post', () => {
@@ -24,14 +26,13 @@ describe('parse', () => {
       openid: 'oP8vYt86psFCDN_YWUaZpPhOQDTk'
     }
 
-    expect(Receiver.parse(token, null, query, undefined)).toBeFalsy()
-    expect(Receiver.parse(token, null, query, 'xxxx')).toBeFalsy()
-    expect(Receiver.parse(token, null, query, '<x></x>')).toBeFalsy()
+    expect(Receiver.parse(config, query, undefined)).toBeFalsy()
+    expect(Receiver.parse(config, query, 'xxxx')).toBeFalsy()
+    expect(Receiver.parse(config, query, '<x></x>')).toBeFalsy()
 
     expect(
       Receiver.parse(
-        token,
-        null,
+        config,
         query,
         '<xml><ToUserName><![CDATA[gh_39993584375c]]></ToUserName>\n<FromUserName><![CDATA[oP8vYt86psFCDN_YWUaZpPhOQDTk]]></FromUserName>\n<CreateTime>1555664951</CreateTime>\n<MsgType><![CDATA[text]]></MsgType>\n<Content><![CDATA[hi]]></Content>\n<MsgId>22271766593385610</MsgId>\n</xml>'
       ).message
@@ -40,14 +41,72 @@ describe('parse', () => {
 })
 
 describe('stringify', () => {
+  test('patch relay message', () => {
+    let patch = Receiver.patchReplyMessage
+    let from = {
+      FromUserName: 'user',
+      ToUserName: 'server'
+    }
+    expect(patch('abc')).toHaveProperty('Content', 'abc')
+    expect(patch('abc')).toHaveProperty('MsgType', 'text')
+    expect(patch('abc', from)).toHaveProperty('FromUserName', 'server')
+    expect(patch('abc', from)).toHaveProperty('ToUserName', 'user')
+    expect(patch('abc')).toHaveProperty('CreateTime')
+
+    let articles = [
+      {
+        Title: 'abc'
+      }
+    ]
+    expect(patch(articles, from)).toHaveProperty('ArticleCount', 1)
+    expect(patch(articles, from).Articles).toEqual(articles)
+    expect(patch({ type: 'video' }, from)).toHaveProperty('MsgType', 'video')
+  })
+
   test('stringify data', () => {
     let txt = Receiver.stringifyData({
       ToUserName: 'to',
       FromUserName: 'from',
-      CreateTime: 12345678
+      CreateTime: 12345678,
+      MsgType: 'music',
+      Music: {
+        Title: 'music',
+        ThumbMediaId: 'media_id'
+      },
+      ArticleCount: 2,
+      Articles: [{ Title: 'Article' }, { Title: 'Article1' }],
+      Articles1: [{ Title: 'Article2' }]
     })
-    expect(txt).toMatch('ToUserName')
-    //console.log('11111', txt)
+    expect(txt).toMatch('<ToUserName><![CDATA[to]]></ToUserName>')
+    expect(txt).toMatch('<CreateTime>12345678</CreateTime>')
+    expect(txt).toMatch('<ThumbMediaId><![CDATA[media_id]]></ThumbMediaId>')
+    expect(txt).toMatch(/<Articles>[\s\n\t]+<item>[\s\n\t]+<Title>/i)
+
+    txt = Receiver.stringifyData({
+      Articles: { item: [{ Title: 'Article' }] }
+    })
+    expect(txt).toMatch(/<Articles>[\s\n\t]+<item>[\s\n\t]+<Title>/i)
+  })
+
+  test('stringify', () => {
+    let from = {
+      FromUserName: 'user',
+      ToUserName: 'server'
+    }
+    let txt = Receiver.stringify({}, {}, from, 'text')
+    expect(txt).toMatch(/^<xml>[\s\n\t]+<MsgType>/i)
+    expect(txt).toMatch('<MsgType><![CDATA[text]]></MsgType>')
+    expect(txt).toMatch('<Content><![CDATA[text]]></Content>')
+    expect(txt).toMatch('<FromUserName><![CDATA[server]]></FromUserName>')
+    txt = Receiver.stringify({}, {}, from, [{ Title: 'art' }])
+    expect(txt).toMatch(/<Articles>[\s\n\t]+<item>[\s\n\t]+<Title>/i)
+    txt = Receiver.stringify({}, {}, from, {
+      type: 'video',
+      Video: {
+        MediaId: 'media_id'
+      }
+    })
+    expect(txt).toMatch(/<Video>[\s\n\t]+<MediaId>/i)
   })
 })
 
